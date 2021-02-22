90de6fb1857c1e80b822d4612378a83e2fb15a72
Eduardo Cáceres <eduherminio@users.noreply.github.com>
2021-02-23 00:39:18 +0100

    Initial commit

---
+++ src/ConsoleRunner/Program.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleRunner;
+
+if (args.Length == 0)
+{
+    const string inputFileName = "a_example.txt";
+    args = new[] { inputFileName };
+}
+
+foreach (var inputFileName in args)
+{
+    new Solver(inputFileName).Solve();
+}
---
+++ src/ConsoleRunner/Solver.cs
@@ -0,0 +1,43 @@
+﻿using FileParser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleRunner
+{
+    public class Solver
+    {
+        private readonly string _inputFile;
+        private readonly string _outputFile;
+
+        public Solver(string inputFileName)
+        {
+            _inputFile = Path.Combine("Inputs", inputFileName);
+            _outputFile = Path.Combine("Outputs", $"output_{inputFileName}");
+
+            Directory.CreateDirectory("Outputs");
+        }
+
+        public void Solve()
+        {
+            ParseInput();
+
+            PrintResult();
+        }
+
+        private void ParseInput()
+        {
+            foreach (var line in new ParsedFile(_inputFile))
+            {
+                Console.WriteLine(line.ToSingleString());
+            }
+        }
+
+        private void PrintResult()
+        {
+            File.WriteAllText(_outputFile, "text");
+        }
+    }
+}
